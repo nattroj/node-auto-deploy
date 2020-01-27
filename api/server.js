ba8b8250ff18e681371f1bae489b2e784bb471d2
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2020-01-26 18:04:59 -0800

    adds simplegit

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const git = require('simple-git')('~/test_projects/scp-fe');
 const server = express();
 
 server.use(express.json());
 
 server.post('/', (req, res) => {
   console.log(req.body);
-  res.json({ message: 'success' })
+  git().pull('origin','master');
+  res.json({ message: 'success' });
 })
 
 module.exports = server;
